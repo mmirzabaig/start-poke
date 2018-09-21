18ff326ffa5b0b666292c52aeb47d5747053ed4d
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-09-21 11:35:52 -0500

    first commit

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,1 +1,10 @@
-
+$('button').on('click', () => {
+  console.log('click works');
+  createSquares(30);
+});
+function createSquares(numberOfSquares) {
+  for ( let i = 0; i <= numberOfSquares; i++ ) {
+    const $div = $('<div/>');
+    $('.squares').append($div);
+  }
+}
