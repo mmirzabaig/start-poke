ee4369415c241abaf397675b70aa0e44e663450c
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-09-21 12:27:52 -0500

    function in progress

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,31 +1,50 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-};
 let colorArray = ['red', 'green', 'blue']
-$('button').on('click', () => {
-  console.log('click works');
-  createSquares(30);
-  // $('.squares').css('background-color', colorArray[getRandomInt(0, colorArray.length)]);
 
-});
-function createSquares(numberOfSquares) {
-  for ( let i = 0; i <= numberOfSquares; i++ ) {
-    applyRandomColor();
-    const $div = $('<div/>');
-    $('.squares').append($div);
-  }
-}
-const applyRandomColor = (square) => {
-  const randomNumber = Math.floor(Math.random() * 3) + 1;
 
-  if (randomNumber === 1){
-    square.css('background-color', 'red')
-  } else if (randomNum == 2) {
-    square.css('background-color', 'blue')
-    else {
-    square.css('background-color', 'green')
+  $('button').on('click', () => {
+    //console.log('click works');
+    // When the user clicks the button, populate the squares
+    createSquares(30);
+
+  });
+
+
+  // Create a function named createSquares, that takes
+  // a parameter of an arbitary number and creates
+  // divs according to the number in the square class
+
+  const createSquares = (numberOfSquares) => {
+
+    for (let i = 0; i < numberOfSquares; i++){
+      // creating squares
+      const $div = $('<div/>').on('click', () => {
+        $('.style').fadeTo('slow', .44);
+      });
+      // this is where the color is applied
+      applyRandomColor($div);
+      // attach to the square class
+      $('.squares').append($div);
+
     }
+  };
+
+  //Now we have a bunch of blue squares,
+  // but we want our squares to have a random,
+  // red, blue, or green color
+  // try to write a function to make that happen.
+
+  const applyRandomColor = (square) => {
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    // im assuming square is a jquery object
+
+    if(randNum === 1){
+      square.css('background-color', 'red');
+    } else if(randNum === 2){
+      square.css('background-color', 'blue');
+    } else {
+      square.css('background-color', 'green');
+    }
+
+
   }
-}
